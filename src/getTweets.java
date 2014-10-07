3b03e86500434535ec86cf9f7c5fe326a92cbe65
Mayur Kale <mayurkale22@gmail.com>
2014-10-07 11:51:20 -0700

    Project start

---
+++ src/getTweets.java
@@ -0,0 +1,11 @@
+/*
+ * This is the program to get recent tweets and output to a file.
+ * Created by Mayur Kale on 10/07/2014
+ * API : Twitter4j
+ */
+
+public class getTweets {
+	public static void main (String [] args) {
+		// Main entry of this application.
+	}
+}
